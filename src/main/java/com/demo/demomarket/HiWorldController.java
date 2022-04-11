02e5f549775e54fd3a5c09499c09a3522af09269
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-04-11 12:40:08 -0500

    adding HiWorldController to project

---
+++ src/main/java/com/demo/demomarket/HiWorldController.java
@@ -0,0 +1,15 @@
+package com.demo.demomarket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HiWorldController {
+
+    @GetMapping("/hola")
+    public  String saludar() {
+        return  "never stop learning";
+    }
+}
