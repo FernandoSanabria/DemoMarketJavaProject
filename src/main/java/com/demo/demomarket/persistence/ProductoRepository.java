67eba89c48d297e572c4b671b464d7b0b44fe8c3
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-04-12 18:28:08 -0500

    specification @Repository to indicate a component that interacts with the database

--- src/main/java/com/demo/demomarket/persistence/ProductoRepository.java
+++ src/main/java/com/demo/demomarket/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.demo.demomarket.persistence;
 
 import com.demo.demomarket.persistence.crud.ProductoCrudRepository;
 import com.demo.demomarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productCrudRepository;
@@ -24,6 +26,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public  Optional<Producto> getProducto( int idProducto) {
+        return  productCrudRepository.findById(idProducto);
+    }
+
+    public Producto save( Producto producto) {
+        return productCrudRepository.save(producto);
+    }
+
+    public void delete(int producto) {
+         productCrudRepository.deleteById(producto);
     }
 
 }
