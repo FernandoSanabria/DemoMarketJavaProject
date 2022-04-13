a317d3709b90b17f99445274f7672d7413d66826
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-04-13 08:48:26 -0500

    adding domain service

---
+++ src/main/java/com/demo/demomarket/domain/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.demo.demomarket.domain.service;
+
+import com.demo.demomarket.domain.Product;
+import com.demo.demomarket.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return  productRepository.getProduct(productId);
+    }
+
+    public Product save(Product product) {
+        return  productRepository.save(product);
+    }
+
+    public boolean delete (int productId) {
+        return getProduct(productId).map(product -> {
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+}
