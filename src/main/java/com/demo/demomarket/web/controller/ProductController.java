599d18dc0c34346abf81eba39f75a664e634a3fc
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-04-13 13:15:54 -0500

    mapping controller methods to http requests

--- src/main/java/com/demo/demomarket/web/controller/ProductController.java
+++ src/main/java/com/demo/demomarket/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.demo.demomarket.web.controller;
 import com.demo.demomarket.domain.Product;
 import com.demo.demomarket.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,24 +14,29 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/all")
     public List<Product> getAll( ) {
        return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId) {
-        return  productService.getProduct(productId);
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int id) {
+        return  productService.getProduct(id);
     }
 
-    public Product save(Product product) {
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
         return  productService.save(product);
     }
 
-    public boolean delete (int productId) {
-        return productService.delete(productId);
+    @DeleteMapping("/delete/{id}")
+    public boolean delete (@PathVariable("id") int id) {
+        return productService.delete(id);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId ) {
-        return productService.getByCategory(categoryId);
+    @GetMapping("/category/{id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("id") int id ) {
+        return productService.getByCategory(id);
     }
 
 }
