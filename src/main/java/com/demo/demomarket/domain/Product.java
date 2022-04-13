8f1189d314057024863821b202c4b183a451bc1b
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-04-12 23:00:20 -0500

    applying data mapper patron

---
+++ src/main/java/com/demo/demomarket/domain/Product.java
@@ -0,0 +1,11 @@
+package com.demo.demomarket.domain;
+
+public class Product {
+    private int productId;
+    private String name;
+    private  int  categoryId;
+    private  double price;
+    private int stock;
+    private boolean active;
+    private Category category;
+}
