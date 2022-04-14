c5978b5129814470e6c46b544e711db977951ca2
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-04-14 12:48:27 -0500

    adding JWT to project

--- src/main/java/com/demo/demomarket/domain/service/DemoUserDetailsService.java
+++ src/main/java/com/demo/demomarket/domain/service/DemoUserDetailsService.java
@@ -1,2 +1,17 @@
-package com.demo.demomarket.domain.service;public class DemoUserDetailsService {
+package com.demo.demomarket.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class DemoUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("Fernando", "{noop}root", new ArrayList<>());
+    }
 }
--- src/main/java/com/demo/demomarket/web/security/SecurityConfig.java
+++ src/main/java/com/demo/demomarket/web/security/SecurityConfig.java
@@ -1,2 +1,19 @@
-package com.demo.demomarket.web.security;public class SecurityConfig {
+package com.demo.demomarket.web.security;
+
+import com.demo.demomarket.domain.service.DemoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DemoUserDetailsService demoUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(demoUserDetailsService);
+    }
 }
